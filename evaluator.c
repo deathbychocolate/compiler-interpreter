02e6223edfc69762d391ff3c6b8839ae8be56136
Seth Piercey <seth.piercey@gmail.com>
2017-07-10 16:38:50 -0300

    it works

--- evaluator.c
+++ evaluator.c
@@ -14,55 +14,57 @@ void hi()
 }
 
 int evaluate(exprs *exprList) {
-	expr *e = exprList->e;
-	switch (e->type) {
-	case eString:
-		printf("%s\n", e->sVal);
-		hi();
-		break;
-	case eIdent:
-		if (strcmp(e->sVal, "+") == 0) {
-			result = 0;
-			op = '+';
-			result = evaluate(exprList->n);
-		}
 
-		if (strcmp(e->sVal, "-") == 0) {
-			result = 0;
-			op = '-';
-			result = evaluate(exprList->n);
-		}
-		break;
-	case eInt:
-		result = e->iVal;
-		while (exprList->n != NULL) {
-			exprList = exprList->n;
-			e = exprList->e;
-			if (e->type == eExprList) {
-				result = evaluate(e->eVal);
+	expr *e = exprList->e;
+
+	switch (e->type) {
+		case eString:
+			printf("%s\n", e->sVal);
+			hi();
+			break;
+		case eIdent:
+			if (strcmp(e->sVal, "+") == 0) {
+				result = 0;
+				op = '+';
+				result = evaluate(exprList->n);
 			}
-			switch (op)
-			{
-			case '+':
-				result += evaluate(exprList);
-				break;
-			case '-':
-				result -= evaluate(exprList);
-				break;
-			default:
-				break;
+
+			if (strcmp(e->sVal, "-") == 0) {
+				result = 0;
+				op = '-';
+				result = evaluate(exprList->n);
 			}
-		}
-		break;
-	case eExprList:
-		if (e->eVal != NULL) {
-			evaluate(e->eVal);
-		}
-		break;
-	default:
-		fatalError("Unknown type");
-		result = 0; /* dead code, assignment shouldn't happen */
-		break;
+			break;
+		case eInt:
+			result = e->iVal;
+			while (exprList->n != NULL) {
+				exprList = exprList->n;
+				e = exprList->e;
+				if (e->type == eExprList) {
+					result = evaluate(e->eVal);
+				}
+				switch (op)
+				{
+					case '+':
+						result += evaluate(exprList);
+						break;
+					case '-':
+						result -= evaluate(exprList);
+						break;
+					default:
+						break;
+				}
+			}
+			break;
+		case eExprList:
+			if (e->eVal != NULL) {
+				evaluate(e->eVal);
+			}
+			break;
+		default:
+			fatalError("Unknown type");
+			result = 0; /* dead code, assignment shouldn't happen */
+			break;
 	}
 	if (e->sVal != NULL) {
 		printf("Node evaluated to: %s\n", e->sVal);
